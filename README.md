716b800583d61b615bd09e3f5db3a46f88df0905
mariarguezp <mariarguezp5@gmail.com>
2022-01-27 12:47:28 +0100

    finish scss

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# React Starter Kit
+**http://beta.adalab.es/modulo-3-evaluacion-final-mariarguezp/**
+
+# Módulo 3. Evaluación final
+
+Desarrollo de una aplicación web de búsqueda de personajes de Harry Potter.  
+La web permite al usuario buscar tanto por el nombre del personaje, como por la casa a la que pertenece y genera una lista de resultados filtrados en función de los parámetros introducidos por el usuario.
+
+### Cómo utilizar la web
+
+Para usar la web haz click en el link que aparece al inicio de este documento.  
+Podrás buscar el personaje escribiendo su nombre en la caja de texto y seleccionando la casa a la que pertenece en el campo de opciones.  
+Al hacer click sobre la tarjeta de cualquiera de los personajes se visualizará su tarjeta detalle, en la que se amplia la información sobre el mismo.  
+Para volver a la lista de resultados haz click en el enlace 'Volver'.
+
+## Construido con 🛠️
+
+<p align="left"> <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="40" height="40"/> </a> <a href="https://sass-lang.com" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg" alt="sass" width="40" height="40"/> </a> <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"/> </a> <a href="https://reactnative.dev/" target="_blank" rel="noreferrer"> <img src="https://reactnative.dev/img/header_logo.svg" alt="reactnative" width="40" height="40"/> </a> </p>
+
+## Autora ✒️
+
+- **María Rodríguez** - [mariarguezp](https://github.com/mariarguezp)
+
+## Licencia 📄
+
+Este proyecto está bajo la Licencia <a href="https://github.com/Adalab/modulo-1-evaluacion-final-mariarguezp/blob/main/LICENSE" target="_blank"><span>MIT License</span></a>
--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -1,4 +1,13 @@
 import { Link } from "react-router-dom";
+import "../styles/layout/CharacterDetail.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
+import { faHeartbeat } from "@fortawesome/free-solid-svg-icons";
+import { faSkull } from "@fortawesome/free-solid-svg-icons";
+import gryffindorEmblem from "../images/Gryffindor-Logo.png";
+import hufflepuffEmblem from "../images/Logo_Hufflepuff_2.png";
+import ravenclawEmblem from "../images/ravenclaw-e1511277281724.png";
+import slytherinEmblem from "../images/Logo_Slytherin_2.png";
 
 function CharacterDetail(props) {
   const getStatus = () => {
@@ -17,38 +26,108 @@ function CharacterDetail(props) {
     }
   };
 
+  const getStatusIcon = () => {
+    if (props.character.status) {
+      return <FontAwesomeIcon icon={faHeartbeat} />;
+    } else {
+      return <FontAwesomeIcon icon={faSkull} />;
+    }
+  };
+
+  const getEmblem = () => {
+    if (props.character.house === "Gryffindor") {
+      return (
+        <li>
+          <img
+            src={gryffindorEmblem}
+            alt={`Emblema de ${props.character.house}`}
+            title={`Emblema de ${props.character.house}`}
+            className="emblem"
+          />
+        </li>
+      );
+    } else if (props.character.house === "Hufflepuff") {
+      return (
+        <li>
+          <img
+            src={hufflepuffEmblem}
+            alt={`Emblema de ${props.character.house}`}
+            title={`Emblema de ${props.character.house}`}
+            className="emblem"
+          />
+        </li>
+      );
+    } else if (props.character.house === "Ravenclaw") {
+      return (
+        <li>
+          <img
+            src={ravenclawEmblem}
+            alt={`Emblema de ${props.character.house}`}
+            title={`Emblema de ${props.character.house}`}
+            className="emblem"
+          />
+        </li>
+      );
+    } else if (props.character.house === "Slytherin") {
+      return (
+        <li>
+          <img
+            src={slytherinEmblem}
+            alt={`Emblema de ${props.character.house}`}
+            title={`Emblema de ${props.character.house}`}
+            className="emblem"
+          />
+        </li>
+      );
+    }
+  };
+
   return (
-    <section>
-      <Link to="/">Volver</Link>
-      <div>
-        <img
-          className="image"
-          src={
-            props.character.image === ""
-              ? "https://via.placeholder.com/210x295/ffffff/666666/?text=HarryPotter"
-              : props.character.image
-          }
-          alt={`Foto de ${props.character.image}`}
-          title={`Foto de ${props.character.image}`}
-        />
-        <ul>
-          <li>{props.character.name}</li>
-          <li>
-            Alias:
-            {props.character.otherNames.length === 0
-              ? " -"
-              : " " + props.character.otherNames}
-          </li>
-          <li>Estatus: {getStatus()}</li>
-          <li>Especie: {props.character.species}</li>
-          <li>
-            Género: {props.character.gender === "female" ? "Mujer" : "Hombre"}
-          </li>
-          <li>
-            Casa: {props.character.house === "" ? "-" : props.character.house}
-          </li>
-        </ul>
-      </div>
+    <section className="detail">
+      <article className="detail__container">
+        <Link to="/" className="detail__link">
+          <FontAwesomeIcon icon={faAngleLeft} className="icon1" />
+          Volver
+        </Link>
+        <div className="detail__card">
+          <img
+            className="detail__card--image"
+            src={
+              props.character.image === ""
+                ? "https://via.placeholder.com/210x295/ffffff/666666/?text=HarryPotter"
+                : props.character.image
+            }
+            alt={`Foto de ${props.character.image}`}
+            title={`Foto de ${props.character.image}`}
+          />
+          <ul className="detail__card--list">
+            <li className="detail__card--listItem1">{props.character.name}</li>
+            <li>
+              <span class="detail__card--itemTitle">Alias:</span>
+              {props.character.otherNames.length === 0
+                ? " -"
+                : " " + props.character.otherNames}
+            </li>
+            <li>
+              <span class="detail__card--itemTitle">Estatus:</span>{" "}
+              {getStatus()} {getStatusIcon()}
+            </li>
+            <li>
+              <span class="detail__card--itemTitle">Especie:</span>{" "}
+              {props.character.species}
+            </li>
+            <li>
+              <span class="detail__card--itemTitle">Género:</span>{" "}
+              {props.character.gender === "female" ? "Mujer" : "Hombre"}
+            </li>
+            <li>
+              <span class="detail__card--itemTitle">Casa:</span>{" "}
+              {props.character.house === "" ? "-" : props.character.house}{" "}
+            </li>
+            {getEmblem()}
+          </ul>
+        </div>
+      </article>
     </section>
   );
 }
